051176fe4451e13bc7626092e669705679cd3605
kylesaldana5 <kylesaldana5@gmail.com>
2018-01-02 18:37:04 -0600

    made json file, first call back

--- dist/bundle.js
+++ dist/bundle.js
@@ -5,14 +5,19 @@ let carnivors = [];
 
 let loadCarnivores = function (callbackToInvoke) {
     const loader = new XMLHttpRequest();
-
-    loader.addEventListener("load", function () {
-        // Set the value of the private array
-        let carnivores = JSON.parse(this.responseText);
-
-        // Now exeute the callback function (`callbackToInvoke`) so that the caller knows that the process is complete. Make sure to pass the carnivore array as an argument.
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", () => {
+    // Set the value of the private array
+    let carnivores = JSON.parse(this.responseText);
+    // Now exeute the callback function (`callbackToInvoke`) so that the caller knows that the process is complete. Make sure to pass the carnivore array as an argument.
+    callbackToInvoke(carnivores);
     });
+    xhr.open("GET", "carnivores.json");
+    xhr.send();
 };
+
+    
+   
 
 module.exports = { loadCarnivores };
 },{}],2:[function(require,module,exports){
